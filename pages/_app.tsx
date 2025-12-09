2063e8a73a29adf9fdd842e928390faa937855fc
Yaman Katby <m.yaman.katby@gmail.com>
2025-12-09 19:46:40 +0300

    Make the page wider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
         />
       </Head>
       <article
-        className="prose mx-auto p-8"
+        className="prose mx-auto p-8 max-w-4xl"
         dir={isArabicPage ? "rtl" : "ltr"}
       >
         <Component {...pageProps} />
