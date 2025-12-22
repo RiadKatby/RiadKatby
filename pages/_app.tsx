ef70d97a3d05d7b7ff9d608e2fff429525128b4e
Mohammad R. Katby <morikapt@gmail.com>
2025-12-22 09:52:16 +0300

    Import KaTeX CSS in _app.tsx
    Added KaTeX CSS for rendering mathematical formulas.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 import "../styles/globals.css";
 
+import 'katex/dist/katex.min.css'
+
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
